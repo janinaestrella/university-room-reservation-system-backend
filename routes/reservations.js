214f6725a2677de90f624c5e93e58c75bc6b6d6b
Janina Estrella <janina.estrella@gmail.com>
2020-06-09 12:39:34 +0800

    RETRIEVE all and specific reservation

--- routes/reservations.js
+++ routes/reservations.js
@@ -4,6 +4,15 @@ const Room = require ('./../models/Room');
 const passport = require('passport');
 require('./../passport-setup');
 
+//middleware for checking authorization
+const isAdmin = (req,res,next) => {
+	if(req.user.isAdmin){
+		return next();
+	} else {
+		return res.status(403).send("Forbidden")
+	}
+}
+
 //CREATE
 router.post('/', passport.authenticate('jwt', {session:false}), (req,res,next) => {
 	//userId
@@ -13,7 +22,8 @@ router.post('/', passport.authenticate('jwt', {session:false}), (req,res,next) =
 	//roomId
 	Room.findById(req.body.roomId)
 	.then(room => {
-
+		
+		//reservation details
 		Reservation.create({
 		userId: userId,
 		reserverName: reserverName,
@@ -32,8 +42,54 @@ router.post('/', passport.authenticate('jwt', {session:false}), (req,res,next) =
 })
 
 //GET ALL
+router.get('/', passport.authenticate('jwt', {session:false}), (req,res,next) => {
+	//if admin is true, view all transactions
+	if(req.user.isAdmin) {
+		Reservation.find()
+		.then(reservations => {
+			res.send(reservations)
+		})
+		.catch(next)
+	//if admin is false, view only his transactions
+	} else {
+		Reservation.find({userId:req.user._id})
+		.then(reservations => {
+			res.send(reservations)
+		})
+		.catch(next)
+	}
+})
 
-//GET SPECIFIC
+
+//GET SPECIFIC BY ID
+router.get('/:id', passport.authenticate('jwt', {session:false}), (req,res,next) => {
+	//if admin is true, view all transactions
+	if(req.user.isAdmin) {
+		Reservation.find()
+		.then(reservations => {
+			res.send(reservations)
+		})
+		.catch(next)
+	//if admin is false, view only his single transactions
+	} else {
+		Reservation.find({
+			_id: req.params.id,
+			userId:req.user._id
+		})
+		.then(reservation => {
+			console.log(reservation.length)
+			//display only users own transaction
+			if(reservation.length !== 0){
+				res.send(reservation) 
+
+			//forbidden to view other users' reservations
+			} else { 
+				return res.status(403).send("Forbidden")
+			}
+		})
+		.catch(next)
+	}
+})
 
 //UPDATE STATUS
 
